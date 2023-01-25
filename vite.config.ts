184d796901702f9412d789bc0fbba60fc33d3628
river <laolei@forkway.cn>
2023-01-25 21:59:47 +0800

    init: init repo

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import {defineConfig} from 'vite';
+import {qwikVite} from '@builder.io/qwik/optimizer';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig(() => {
+    return {
+        plugins: [
+            qwikVite(),
+            tsconfigPaths(),
+        ],
+        preview: {
+            headers: {
+                'Cache-Control': 'public, max-age=600',
+            },
+        },
+    };
+});
